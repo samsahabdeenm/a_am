0754081fe0644c831e0b3927fd414ffc350dc765
samsahabdeenm <samsahabdeen.m@gmail.com>
2026-03-30 23:39:34 +0530

    Add files via upload

---
+++ puravigal/js/1005.js
@@ -0,0 +1,32 @@
+(function (window, document) {
+  "use strict";
+
+  var NODE_ID = "1005";
+
+  function ready(callback) {
+    if (window.Puravi && typeof window.Puravi.onReady === "function") {
+      window.Puravi.onReady(callback);
+      return;
+    }
+
+    if (document.readyState === "loading") {
+      document.addEventListener("DOMContentLoaded", callback);
+      return;
+    }
+
+    callback();
+  }
+
+  function init() {
+    if (!document.body || document.body.getAttribute("data-node") !== NODE_ID) {
+      return;
+    }
+
+    Array.prototype.slice.call(document.querySelectorAll("[data-signup-sequence]")).forEach(function (element, index) {
+      element.style.setProperty("--puravi-animate-delay", ((index + 1) * 75) + "ms");
+    });
+  }
+
+  ready(init);
+})(window, document);
+
